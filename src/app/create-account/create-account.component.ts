6787fb3e9ca80ccdc09eb6d890d529009747f86e
LeDarkPampa <144164398+LeDarkPampa@users.noreply.github.com>
2023-09-13 16:42:05 +0200

    add updatePassword
    add updatePassword

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,10 +1,6 @@
-import {ChangeDetectorRef, Component, NgZone} from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {AuthentificationService} from "../services/authentification.service";
-import {SseService} from "../services/sse.service";
-import {DialogService} from "primeng/dynamicdialog";
-import {DeckService} from "../services/deck.service";
-import {Router} from "@angular/router";
+import {IUtilisateur} from "../interfaces/IUtilisateur";
 
 @Component({
   selector: 'app-create-account',
@@ -17,8 +13,21 @@ export class CreateAccountComponent {
     password: ''
   };
 
-  constructor(private http: HttpClient) {
+  // @ts-ignore
+  selectedUser: IUtilisateur;
+  newPassword: string = '';
+  utilisateurs: IUtilisateur[] = [];
 
+  constructor(private http: HttpClient) {
+    this.http.get<IUtilisateur[]>('https://pampacardsback-57cce2502b80.herokuapp.com/api/users').subscribe({
+      next: data => {
+        data.forEach(user => this.utilisateurs.push(user));
+      },
+      error: error => {
+        console.error('There was an error!', error);
+        alert('Erreur lors de la récupération des utilisateurs');
+      }
+    })
   }
 
   onSubmit() {
@@ -37,5 +46,29 @@ export class CreateAccountComponent {
       pseudo: '',
       password: ''
     };
+  }
+
+  onChangePassword() {
+
+    this.selectedUser.password = this.newPassword;
+
+    this.http.post<any>('https://pampacardsback-57cce2502b80.herokuapp.com/api/updatePassword', this.selectedUser).subscribe({
+      next: response => {
+        alert('Mot de passe modifié');
+      },
+      error: error => {
+        console.error('There was an error!', error);
+        alert('Erreur lors de la modification');
+      }
+    });
+
+    // Réinitialisation du formulaire après soumission
+    this.user = {
+      pseudo: '',
+      password: ''
+    };
+
+    // Réinitialisation du formulaire après soumission
+    this.newPassword = '';
   }
 }
