9224d80966185d8f83508bbbed64e8b78b676501
LeDarkPampa <144164398+LeDarkPampa@users.noreply.github.com>
2023-09-22 23:09:24 +0200

    Remove boutique et ajout mon compte
    Remove boutique et ajout mon compte

--- src/app/mon-compte/mon-compte.component.ts
+++ src/app/mon-compte/mon-compte.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {IUtilisateur} from "../interfaces/IUtilisateur";
+import {HttpClient} from "@angular/common/http";
+import {AuthentificationService} from "../services/authentification.service";
 
 @Component({
   selector: 'app-mon-compte',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./mon-compte.component.css']
 })
 export class MonCompteComponent {
+  user = {
+    pseudo: '',
+    password: ''
+  };
 
+  utilisateur: IUtilisateur;
+  newPassword: string = '';
+
+  constructor(private http: HttpClient, private authService: AuthentificationService) {
+    this.utilisateur = this.authService.user;
+  }
+
+  onChangePassword() {
+    if (this.utilisateur) {
+      this.utilisateur.password = this.newPassword;
+
+      this.http.post<any>('https://pampacardsback-57cce2502b80.herokuapp.com/api/updatePassword', this.utilisateur).subscribe({
+        next: response => {
+          alert('Mot de passe modifié');
+        },
+        error: error => {
+          console.error('There was an error!', error);
+          alert('Erreur lors de la modification');
+        }
+      });
+
+      // Réinitialisation du formulaire après soumission
+      this.newPassword = '';
+    } else {
+      console.error('Utilisateur non trouvé');
+    }
+  }
 }
