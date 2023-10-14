8e39aa15656c1cb648e3151078dc2fca5196385b
LeDarkPampa <ledarkpampa@gmail.com>
2023-10-14 13:17:38 +0200

    Fixer affichage des news tant qu'elles sont pas chargées
    Fixer affichage des news tant qu'elles sont pas chargées

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {IType} from "../interfaces/IType";
+import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {AuthentificationService} from "../services/authentification.service";
 
