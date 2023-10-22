a21146218d697cfa34b116a92b860bc64c1549c3
LeDarkPampa <ledarkpampa@gmail.com>
2023-10-22 12:21:32 +0200

    ajout tableau résultat parties
    ajout tableau résultat parties

--- src/app/administration/administration-parties/administration-parties.component.ts
+++ src/app/administration/administration-parties/administration-parties.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {IPartie} from "../../interfaces/IPartie";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {IResultatPartie} from "../../interfaces/IResultatPartie";
 
 @Component({
   selector: 'app-administration-parties',
@@ -11,12 +12,24 @@ import {Router} from "@angular/router";
 export class AdministrationPartiesComponent {
 
   // @ts-ignore
-  parties: IPartie[];
+  partiesEnCours: IPartie[];
+  // @ts-ignore
+  resultatsParties: IResultatPartie[];
 
   constructor(private http: HttpClient, private router: Router) {
     this.http.get<IPartie[]>('https://pampacardsback-57cce2502b80.herokuapp.com/api/partiesEnCours').subscribe({
       next: data => {
-        this.parties = data;
+        this.partiesEnCours = data;
+      },
+      error: error => {
+        console.error('There was an error!', error);
+        alert('Erreur lors de la récupération des parties en cours');
+      }
+    });
+
+    this.http.get<IPartie[]>('https://pampacardsback-57cce2502b80.herokuapp.com/api/resultatsPartiesTerminees').subscribe({
+      next: data => {
+        this.partiesEnCours = data;
       },
       error: error => {
         console.error('There was an error!', error);
---
+++ src/app/interfaces/IResultatPartie.ts
@@ -0,0 +1,10 @@
+export interface IResultatPartie {
+  partieId: number;
+  vainqueur: string;
+  pseudoJoueurUn: string;
+  scoreJoueurUn: number;
+  nomDeckJoueurUn: string;
+  pseudoJoueurDeux: string;
+  scoreJoueurDeux: number;
+  nomDeckJoueurDeux: string;
+}
