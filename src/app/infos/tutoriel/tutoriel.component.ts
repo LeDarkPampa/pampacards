7af3ad9faefe9dbe6a71fa1dc0523286619d9b61
LeDarkPampa <ledarkpampa@gmail.com>
2023-11-03 02:29:15 +0100

    Faire dans info une page tuto du jeu
    Faire dans info une page tuto du jeu

--- src/app/infos/tutoriel/tutoriel.component.ts
+++ src/app/infos/tutoriel/tutoriel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tutoriel',
   templateUrl: './tutoriel.component.html',
-  styleUrls: ['./tutoriel.component.css']
+  styleUrls: ['./tutoriel.component.css', '../../app.component.css']
 })
 export class TutorielComponent {
 
