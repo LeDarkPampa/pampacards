1e122ca30452f5992798d0d0cc9e693d63029acf
LeDarkPampa <ledarkpampa@gmail.com>
2023-11-09 21:56:28 +0100

    fix
    fix

--- src/app/interfaces/LigueTournoiStatutEnum.ts
+++ src/app/interfaces/LigueTournoiStatutEnum.ts
@@ -1,8 +1,8 @@
 export enum LigueTournoiStatutEnum {
   A_VENIR = "A venir",
-  EN_ATTENTE = "En attente",
   INSCRIPTIONS_OUVERTES = "Inscriptions ouvertes",
   INSCRIPTIONS_FERMEES = "Inscriptions fermées",
+  EN_ATTENTE = "En attente",
   EN_COURS = "En cours",
   TERMINE = "Terminé"
 }
