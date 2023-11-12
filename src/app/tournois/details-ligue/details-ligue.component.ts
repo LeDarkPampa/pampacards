d7a2c57f6b7b0aa688c21d1b9f287783dd8e0e58
LeDarkPampa <ledarkpampa@gmail.com>
2023-11-12 21:09:13 +0100

    Ecran détail ligue/tournoi : Polling régulier qui retourne l’état de la ligue / du tournoi
    Ecran détail ligue/tournoi : Polling régulier qui retourne l’état de la ligue / du tournoi

--- src/app/tournois/details-ligue/details-ligue.component.ts
+++ src/app/tournois/details-ligue/details-ligue.component.ts
@@ -1,48 +1,58 @@
-import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
-import {AuthentificationService} from "../../services/authentification.service";
-import {DialogService} from "primeng/dynamicdialog";
-import {SseService} from "../../services/sse.service";
 import {ILigue} from "../../interfaces/ILigue";
 import {ICompetitionParticipant} from "../../interfaces/ICompetitionParticipant";
+import {interval, startWith, Subscription, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-details-ligue',
   templateUrl: './details-ligue.component.html',
   styleUrls: ['./details-ligue.component.css', '../../app.component.css']
 })
-export class DetailsLigueComponent implements OnInit {
+export class DetailsLigueComponent implements OnInit, OnDestroy {
 
   ligueId: number = 0;
   // @ts-ignore
   ligue: ILigue;
   players: ICompetitionParticipant[] = [];
   private BACKEND_URL = "https://pampacardsback-57cce2502b80.herokuapp.com";
+  private subscription: Subscription | undefined;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthentificationService,
-              private dialogService: DialogService, private zone: NgZone,
-              private sseService: SseService, private cd: ChangeDetectorRef) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.ligueId = params['id'];
-      this.getLigue();
-      this.cd.detectChanges();
+      this.subscription = interval(5000)
+        .pipe(
+          startWith(0),
+          switchMap(() => this.http.get<ILigue>(`${this.BACKEND_URL}/ligues/ligue?id=` + this.ligueId))
+        )
+        .subscribe({
+          next: ligue => {
+            this.ligue = ligue;
+            this.players = this.ligue.participants;
+          },
+          error: error => {
+            console.error('There was an error!', error);
+          }
+        });
     });
   }
 
-  private getLigue() {
-    this.http.get<ILigue>(`${this.BACKEND_URL}/ligues/ligue?id=` + this.ligueId).subscribe({
-      next: ligue => {
-        this.ligue = ligue;
-        this.players = this.ligue.participants;
-      },
-      error: error => {
-        console.error('There was an error!', error);
-      }
-    });
+  hasAffrontement(joueurId1: number, joueurId2: number): boolean {
+    return this.ligue.affrontements.some(affrontement =>
+      (affrontement.joueur1Id === joueurId1 && affrontement.joueur2Id === joueurId2) ||
+      (affrontement.joueur1Id === joueurId2 && affrontement.joueur2Id === joueurId1)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
--- src/app/tournois/details-tournoi/details-tournoi.component.ts
+++ src/app/tournois/details-tournoi/details-tournoi.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 import {AuthentificationService} from "../../services/authentification.service";
@@ -6,13 +6,14 @@ import {DialogService} from "primeng/dynamicdialog";
 import {SseService} from "../../services/sse.service";
 import {ITournoi} from "../../interfaces/ITournoi";
 import {ICompetitionParticipant} from "../../interfaces/ICompetitionParticipant";
+import {interval, startWith, Subscription, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-details-tournoi',
   templateUrl: './details-tournoi.component.html',
   styleUrls: ['./details-tournoi.component.css', '../../app.component.css']
 })
-export class DetailsTournoiComponent implements OnInit {
+export class DetailsTournoiComponent implements OnInit, OnDestroy {
 
   tournoiId: number = 0;
   // @ts-ignore
@@ -21,31 +22,37 @@ export class DetailsTournoiComponent implements OnInit {
   players: ICompetitionParticipant[] = [];
 
   private BACKEND_URL = "https://pampacardsback-57cce2502b80.herokuapp.com";
+  private subscription: Subscription | undefined;
 
-
-  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthentificationService,
-              private dialogService: DialogService, private zone: NgZone,
-              private sseService: SseService, private cd: ChangeDetectorRef) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.tournoiId = params['id'];
-      this.getTournoi();
-      this.cd.detectChanges();
+
+      this.subscription = interval(5000)
+        .pipe(
+          startWith(0),
+          switchMap(() => this.http.get<ITournoi>(`${this.BACKEND_URL}/tournois/tournoi?id=` + this.tournoiId))
+        )
+        .subscribe({
+          next: tournoi => {
+            this.tournoi = tournoi;
+            this.players = this.tournoi.participants;
+          },
+          error: error => {
+            console.error('There was an error!', error);
+          }
+        });
     });
   }
 
-  private getTournoi() {
-    this.http.get<ITournoi>(`${this.BACKEND_URL}/tournois/tournoi?id=` + this.tournoiId).subscribe({
-      next: tournoi => {
-        this.tournoi = tournoi;
-        this.players = this.tournoi.participants;
-      },
-      error: error => {
-        console.error('There was an error!', error);
-      }
-    });
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
+
 }
