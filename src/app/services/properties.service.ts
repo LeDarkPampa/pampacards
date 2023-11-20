82d4c23286d228d045320a5b5f2bf31604f6671f
LeDarkPampa <ledarkpampa@gmail.com>
2023-11-20 01:54:39 +0100

    try to fix test values

--- src/app/services/properties.service.ts
+++ src/app/services/properties.service.ts
@@ -6,20 +6,29 @@ import {IPropertie} from "../interfaces/IPropertie";
   providedIn: 'root'
 })
 export class PropertiesService {
-
   testModeOn = false;
 
   constructor(private http: HttpClient) {
-
+    this.loadProperties();
   }
 
   loadProperties() {
+    const storedTestMode = localStorage.getItem('testModeOn');
+    if (storedTestMode) {
+      this.testModeOn = JSON.parse(storedTestMode);
+    } else {
+      this.fetchProperties();
+    }
+  }
+
+  private fetchProperties() {
     this.http.get<IPropertie[]>('https://pampacardsback-57cce2502b80.herokuapp.com/api/properties').subscribe({
       next: (data: IPropertie[]) => {
         const showTestProperty = data.find(property => property.code === 'SHOW_TEST');
 
         if (showTestProperty) {
           this.testModeOn = showTestProperty.value;
+          localStorage.setItem('testModeOn', JSON.stringify(this.testModeOn));
         }
       },
       error: error => {
@@ -30,6 +39,11 @@ export class PropertiesService {
   }
 
   isTestModeOn() {
-    return this.testModeOn;
+    const storedTestMode = localStorage.getItem('testModeOn');
+    if (storedTestMode) {
+      this.testModeOn = JSON.parse(storedTestMode);
+    } else {
+      this.fetchProperties();
+    }
   }
 }
