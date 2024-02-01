73d4a178782a240c39400e451510611fec37e61e
LeDarkPampa <ledarkpampa@gmail.com>
2024-02-01 17:23:57 +0100

    update page accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
@@ -7,9 +8,9 @@ import {Component} from '@angular/core';
 })
 export class AccueilComponent {
 
-  constructor() {
+  constructor(private router: Router) {}
+
+  navigateTo(link: string) {
 
   }
-
 }
-
