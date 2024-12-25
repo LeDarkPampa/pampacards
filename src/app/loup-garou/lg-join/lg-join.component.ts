d88a5775ddc0ce6aeb3d43f840820501da25df93
LeDarkPampa <ledarkpampa@gmail.com>
2024-12-25 15:39:35 +0100

    update

--- src/app/loup-garou/lg-join/lg-join.component.ts
+++ src/app/loup-garou/lg-join/lg-join.component.ts
@@ -17,8 +17,14 @@ export class LgJoinComponent {
   rejoindre() {
     this.lgPartieService.joinGame(this.code, this.nomJoueur).subscribe(
       (response) => {
-        console.log('Partie rejointe avec succès :', response.gameId);
-        this.router.navigate(['lg/game', response.gameId, response.playerId]);
+        if (response.gameId && response.playerId) {
+
+          const url = `/lg/game/${response.gameId}/${response.playerId}`;
+          this.router.navigateByUrl(url);
+          console.log('Partie rejointe avec succès :', response.gameId);
+        } else {
+          console.error('Les paramètres de la partie ou du joueur sont manquants.');
+        }
       },
       (error) => {
         console.error('Erreur lors de la récupération de la partie :', error);
