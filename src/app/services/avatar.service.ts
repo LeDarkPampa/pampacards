523114dfd0e9b285613a81561d0da98510fd9d88
LeDarkPampa <ledarkpampa@gmail.com>
2024-12-28 22:55:40 +0100

    fix recup avatar

--- src/app/services/avatar.service.ts
+++ src/app/services/avatar.service.ts
@@ -19,7 +19,21 @@ export class AvatarService extends ApiService {
 
   getAvatar(): Observable<Avatar> {
     const userId = this.authentificationService.getUserId();
-    return this.http.get<Avatar>(`/api/avatar?userId=${userId}`);
+
+    return this.http.get<Avatar>(this.API_URL + `/avatar?userId=${userId}`)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 200) {
+            // La réponse peut être vide ou incorrecte
+            console.error("Réponse mal formatée ou vide", error);
+          } else if (error.error instanceof ErrorEvent) {
+            console.error("Erreur côté client ou réseau", error.error.message);
+          } else {
+            console.error(`Erreur serveur : ${error.status}, message : ${error.message}`);
+          }
+          return throwError('Erreur lors de la récupération de l\'avatar');
+        })
+      );
   }
 
   getAvatarByUserId(userId: number): Observable<Avatar> {
