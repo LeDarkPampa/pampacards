36437a1cb72a7295e2708aeb757c0a5431151d28
LeDarkPampa <ledarkpampa@gmail.com>
2026-04-06 01:57:39 +0200

    update detail statuts

--- src/app/tutoriel/details-statuts/details-statuts.component.ts
+++ src/app/tutoriel/details-statuts/details-statuts.component.ts
@@ -1,9 +1,12 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {Type} from "../../classes/cartes/Type";
-import {CartePartie} from "../../classes/cartes/CartePartie";
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Clan } from '../../classes/cartes/Clan';
+import { Type } from '../../classes/cartes/Type';
+import { CartePartie } from '../../classes/cartes/CartePartie';
 import { ReferentielService } from '../../services/referentiel.service';
 import { UiMessageService } from '../../services/ui-message.service';
 import { CARTE_VIEW_MSG } from '../../core/messages/domain.messages';
+
+const DEMO_CARTE_ID = 1110;
 
 @Component({
   selector: 'app-details-statuts',
@@ -12,176 +15,73 @@ import { CARTE_VIEW_MSG } from '../../core/messages/domain.messages';
 })
 export class DetailsStatutsComponent implements OnInit {
 
-  // @ts-ignore
-  normale: CartePartie;
-  // @ts-ignore
-  bouclier: CartePartie;
-  // @ts-ignore
-  insensible: CartePartie;
-  // @ts-ignore
-  corrompu: CartePartie;
-  // @ts-ignore
-  silence: CartePartie;
-  // @ts-ignore
-  indestructible: CartePartie;
-  // @ts-ignore
-  prison: CartePartie;
-  private clanCorrompu: Type = {
-    id: 0,
-    nom: 'Corrompu'
-  };
+  normale?: CartePartie;
+  bouclier?: CartePartie;
+  insensible?: CartePartie;
+  corrompu?: CartePartie;
+  silence?: CartePartie;
+  indestructible?: CartePartie;
+  prison?: CartePartie;
 
-  private typeCorrompu: Type = {
-    id: 0,
-    nom: 'Corrompu'
-  };
+  loading = true;
+  loadFailed = false;
+
+  private readonly clanCorrompu: Clan = { id: 0, nom: 'Corrompu' };
+  private readonly typeCorrompu: Type = { id: 0, nom: 'Corrompu' };
 
   constructor(
     private referentielService: ReferentielService,
     private cd: ChangeDetectorRef,
     private uiMessage: UiMessageService
-  ) {
-  }
+  ) {}
 
-  ngOnInit() {
-    this.referentielService.getCarteById(1110).subscribe({
-      next: (data) => {
-        const carte = data as CartePartie;
-        carte.bouclier = false;
-        carte.insensible = false;
-        carte.silence = false;
-        carte.prison = false;
-        this.normale = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: false,
-          insensible: false,
-          prison: false,
-          cartePartieId: 0
-        }
-        this.bouclier = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: true,
-          insensible: false,
-          prison: false,
-          cartePartieId: 0
-        };
+  ngOnInit(): void {
+    this.referentielService.getCarteById(DEMO_CARTE_ID).subscribe({
+      next: data => {
+        const src = data as CartePartie;
+        const base = this.demoBase(src);
+        this.normale = base;
+        this.bouclier = { ...base, bouclier: true };
+        this.indestructible = { ...base, bouclier: true };
         this.corrompu = {
-          id: carte.id,
-          nom: carte.nom,
+          ...base,
           clan: this.clanCorrompu,
-          type: this.typeCorrompu,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: false,
-          insensible: false,
-          prison: false,
-          cartePartieId: 0
+          type: this.typeCorrompu
         };
-        this.insensible = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: false,
-          insensible: true,
-          prison: false,
-          cartePartieId: 0
-        };
-        this.silence = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: true,
-          bouclier: false,
-          insensible: false,
-          prison: false,
-          cartePartieId: 0
-        };
-        this.indestructible = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: true,
-          insensible: false,
-          prison: false,
-          cartePartieId: 0
-        };
-        this.prison = {
-          id: carte.id,
-          nom: carte.nom,
-          clan: carte.clan,
-          type: carte.type,
-          rarete: carte.rarete,
-          effet: carte.effet,
-          puissance: carte.puissance,
-          image_path: carte.image_path,
-          diffPuissanceInstant: carte.diffPuissanceInstant,
-          diffPuissanceContinue: carte.diffPuissanceContinue,
-          released: carte.released,
-          silence: false,
-          bouclier: false,
-          insensible: false,
-          prison: true,
-          cartePartieId: 0
-        };
+        this.insensible = { ...base, insensible: true };
+        this.silence = { ...base, silence: true };
+        this.prison = { ...base, prison: true };
+        this.loading = false;
+        this.loadFailed = false;
         this.cd.detectChanges();
       },
       error: () => {
+        this.loading = false;
+        this.loadFailed = true;
         this.uiMessage.error(CARTE_VIEW_MSG.LOAD_ERR);
+        this.cd.detectChanges();
       }
     });
+  }
+
+  private demoBase(carte: CartePartie): CartePartie {
+    return {
+      id: carte.id,
+      nom: carte.nom,
+      clan: carte.clan,
+      type: carte.type,
+      rarete: carte.rarete,
+      effet: carte.effet,
+      puissance: carte.puissance,
+      image_path: carte.image_path,
+      diffPuissanceInstant: carte.diffPuissanceInstant,
+      diffPuissanceContinue: carte.diffPuissanceContinue,
+      released: carte.released,
+      silence: false,
+      bouclier: false,
+      insensible: false,
+      prison: false,
+      cartePartieId: 0
+    };
   }
 }
